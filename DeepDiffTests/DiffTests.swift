293b0ca22403aff7d5710fedb948f3eb4b8debc0
Khoa Pham <onmyway133@gmail.com>
2017-10-30 21:28:55 +0100

    Add testEmpty, testAllInsert, testAllDelete

--- DeepDiffTests/DiffTests.swift
+++ DeepDiffTests/DiffTests.swift
@@ -2,6 +2,27 @@ import XCTest
 import DeepDiff
 
 class DiffTests: XCTestCase {
+  func testEmpty() {
+    let old: [String] = []
+    let new: [String] = []
+    let changes = diff(old: old, new: new)
+    XCTAssertEqual(changes.count, 0)
+  }
+
+  func testAllInsert() {
+    let old = Array("")
+    let new = Array("abc")
+    let changes = diff(old: old, new: new)
+    XCTAssertEqual(changes.count, 3)
+  }
+
+  func testAllDelete() {
+    let old = Array("abc")
+    let new = Array("")
+    let changes = diff(old: old, new: new)
+    XCTAssertEqual(changes.count, 3)
+  }
+
   func test1() {
     let old = [1, 2, 3]
     let new = [1, 3, 4]
