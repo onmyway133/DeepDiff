6227653cb1f724f7d7013364c6bb059f5154072c
Khoa Pham <onmyway133@gmail.com>
2017-10-31 10:52:19 +0100

    Set baselines

--- DeepDiffTests/PerformanceTests.swift
+++ DeepDiffTests/PerformanceTests.swift
@@ -2,8 +2,8 @@ import XCTest
 import DeepDiff
 
 class PerformanceTests: XCTestCase {
-  func test4Items_Delete2_Add2() {
-    let data = generate(count: 4, removeRange: 0..<2, adding: true)
+  func test4Items_Replace2() {
+    let data = generate(count: 4, removeRange: 0..<2, addRange: 2..<4)
 
     measure {
       let changes = diff(old: data.old, new: data.new)
@@ -11,8 +11,8 @@ class PerformanceTests: XCTestCase {
     }
   }
 
-  func test100Items_Delete50_Add50() {
-    let data = generate(count: 100, removeRange: 0..<50, adding: true)
+  func test100Items_Replace50() {
+    let data = generate(count: 100, removeRange: 0..<50, addRange: 50..<100)
 
     measure {
       let changes = diff(old: data.old, new: data.new)
@@ -20,8 +20,26 @@ class PerformanceTests: XCTestCase {
     }
   }
 
-  func test1000Items_Delete100_Add100() {
-    let data = generate(count: 1000, removeRange: 100..<200, adding: true)
+  func test1000Items_Replace100() {
+    let data = generate(count: 1000, removeRange: 100..<200, addRange: 799..<899)
+
+    measure {
+      let changes = diff(old: data.old, new: data.new)
+      XCTAssertEqual(changes.count, 100)
+    }
+  }
+
+  func test1000Items_Delete100() {
+    let data = generate(count: 1000, removeRange: 100..<200)
+
+    measure {
+      let changes = diff(old: data.old, new: data.new)
+      XCTAssertEqual(changes.count, 100)
+    }
+  }
+
+  func test1000Items_Add100() {
+    let data = generate(count: 1000, addRange: 999..<1099)
 
     measure {
       let changes = diff(old: data.old, new: data.new)
@@ -30,11 +48,31 @@ class PerformanceTests: XCTestCase {
   }
 
   func test10000Items_Delete1000() {
-    let data = generate(count: 10000, removeRange: 1000..<2000, adding: false)
+    let data = generate(count: 10000, removeRange: 1000..<2000)
 
     measure {
       let changes = diff(old: data.old, new: data.new)
       XCTAssertEqual(changes.count, 1000)
+    }
+  }
+
+  func test100000Items_AllInsert() {
+    let old = [String]()
+    let new = Array(repeatElement(UUID().uuidString, count: 100000))
+
+    measure {
+      let changes = diff(old: old, new: new)
+      XCTAssertEqual(changes.count, 100000)
+    }
+  }
+
+  func test100000Items_AllDelete() {
+    let old = Array(repeatElement(UUID().uuidString, count: 100000))
+    let new = [String]()
+
+    measure {
+      let changes = diff(old: old, new: new)
+      XCTAssertEqual(changes.count, 100000)
     }
   }
 
@@ -43,15 +81,20 @@ class PerformanceTests: XCTestCase {
   /// Generate new by removing some items from old
   /// Use UUID to generate all same items, because of repeating
   /// If adding, add more items to new with new generated UUID
-  func generate(count: Int, removeRange: Range<Int>, adding: Bool) -> (old: Array<String>, new: Array<String>) {
+  func generate(count: Int, removeRange: Range<Int>? = nil, addRange: Range<Int>? = nil)
+    -> (old: Array<String>, new: Array<String>) {
+
     let old = Array(repeating: UUID().uuidString, count: count)
     var new = old
-    new.removeSubrange(removeRange)
 
-    if adding {
+    if let removeRange = removeRange {
+      new.removeSubrange(removeRange)
+    }
+
+    if let addRange = addRange {
       new.insert(
-        contentsOf: Array(repeating: UUID().uuidString, count: removeRange.count),
-        at: count-removeRange.count
+        contentsOf: Array(repeating: UUID().uuidString, count: addRange.count),
+        at: addRange.lowerBound
       )
     }
 
