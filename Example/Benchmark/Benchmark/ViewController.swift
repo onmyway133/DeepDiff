fc71985235298935bd4c17bd70236f4bd531c4ae
Khoa Pham <onmyway133@gmail.com>
2018-01-04 10:41:09 +0100

    Add Benchmark

---
+++ Example/Benchmark/Benchmark/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Benchmark
+//
+//  Created by Khoa Pham on 04.01.2018.
+//  Copyright © 2018 Fantageek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+}
+
