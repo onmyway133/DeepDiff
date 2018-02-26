1a120bb574569013a58df235c02800a5e51eb07e
Gungor Basa <gungorbasa@Gungors-iMac.local>
2018-02-26 13:59:33 +0300

    Texture example is added.

---
+++ Example/DeepDiffDemo/DeepDiffDemo/Sources/Texture/ASTableNodeExtension.swift
@@ -0,0 +1,55 @@
+//
+//  ASTableNodeExtension.swift
+//  DeepDiffDemo
+//
+//  Created by Gungor Basa on 26.02.2018.
+//  Copyright © 2018 Hyper Interaktiv AS. All rights reserved.
+//
+
+import AsyncDisplayKit
+import DeepDiff
+
+extension ASTableNode {
+
+    /// Animate reload in a batch update
+    ///
+    /// - Parameters:
+    ///   - changes: The changes from diff
+    ///   - section: The section that all calculated IndexPath belong
+    ///   - completion: Called when operation completes
+    public func reload<T: Hashable>(
+        changes: [Change<T>],
+        section: Int = 0,
+        completion: @escaping (Bool) -> Void) {
+
+        let changesWithIndexPath = IndexPathConverter().convert(changes: changes, section: section)
+
+        // reloadRows needs to be called outside the batch
+        performBatchUpdates({
+            internalBatchUpdates(changesWithIndexPath: changesWithIndexPath)
+        }, completion: completion)
+
+        changesWithIndexPath.replaces.executeIfPresent {
+            self.reloadRows(at: $0, with: .automatic)
+        }
+    }
+
+    // MARK: - Helper
+
+    private func internalBatchUpdates(changesWithIndexPath: ChangeWithIndexPath) {
+        changesWithIndexPath.deletes.executeIfPresent {
+            deleteRows(at: $0, with: .automatic)
+        }
+
+        changesWithIndexPath.inserts.executeIfPresent {
+            insertRows(at: $0, with: .automatic)
+        }
+
+        changesWithIndexPath.moves.executeIfPresent {
+            $0.forEach { move in
+                moveRow(at: move.from, to: move.to)
+            }
+        }
+    }
+}
+
--- Sources/iOS/IndexPathConverter.swift
+++ Sources/iOS/IndexPathConverter.swift
@@ -1,35 +1,39 @@
 import Foundation
 
-struct ChangeWithIndexPath {
-  let inserts: [IndexPath]
-  let deletes: [IndexPath]
-  let replaces: [IndexPath]
-  let moves: [(from: IndexPath, to: IndexPath)]
+public struct ChangeWithIndexPath {
+    public let inserts: [IndexPath]
+    public let deletes: [IndexPath]
+    public let replaces: [IndexPath]
+    public let moves: [(from: IndexPath, to: IndexPath)]
 }
 
-final class IndexPathConverter {
-  func convert<T>(changes: [Change<T>], section: Int) -> ChangeWithIndexPath {
-    let inserts = changes.flatMap({ $0.insert }).map({ $0.index.toIndexPath(section: section) })
-    let deletes = changes.flatMap({ $0.delete }).map({ $0.index.toIndexPath(section: section) })
-    let replaces = changes.flatMap({ $0.replace }).map({ $0.index.toIndexPath(section: section) })
-    let moves = changes.flatMap({ $0.move }).map({
-      (
-        from: $0.fromIndex.toIndexPath(section: section),
-        to: $0.toIndex.toIndexPath(section: section)
-      )
-    })
+public class IndexPathConverter {
+    public init() {
 
-    return ChangeWithIndexPath(
-      inserts: inserts,
-      deletes: deletes,
-      replaces: replaces,
-      moves: moves
-    )
-  }
+    }
+    
+    public func convert<T>(changes: [Change<T>], section: Int) -> ChangeWithIndexPath {
+        let inserts = changes.flatMap({ $0.insert }).map({ $0.index.toIndexPath(section: section) })
+        let deletes = changes.flatMap({ $0.delete }).map({ $0.index.toIndexPath(section: section) })
+        let replaces = changes.flatMap({ $0.replace }).map({ $0.index.toIndexPath(section: section) })
+        let moves = changes.flatMap({ $0.move }).map({
+            (
+                from: $0.fromIndex.toIndexPath(section: section),
+                to: $0.toIndex.toIndexPath(section: section)
+            )
+        })
+        
+        return ChangeWithIndexPath(
+            inserts: inserts,
+            deletes: deletes,
+            replaces: replaces,
+            moves: moves
+        )
+    }
 }
 
 extension Int {
-  fileprivate func toIndexPath(section: Int) -> IndexPath {
-    return IndexPath(item: self, section: section)
-  }
+    fileprivate func toIndexPath(section: Int) -> IndexPath {
+        return IndexPath(item: self, section: section)
+    }
 }
