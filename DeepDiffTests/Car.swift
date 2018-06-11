7d9929323844be2803fd38670991045abefb6be9
Khoa Pham <onmyway133@gmail.com>
2018-06-11 16:39:01 +0200

    Add Car

---
+++ DeepDiffTests/Car.swift
@@ -0,0 +1,25 @@
+//
+//  Car.swift
+//  DeepDiff
+//
+//  Created by Khoa Pham on 11.06.2018.
+//  Copyright © 2018 Khoa Pham. All rights reserved.
+//
+
+import Foundation
+
+struct Car: Equatable {
+  let id: Int
+  let name: String
+  let travelledMiles: Int
+
+  var hashValue: Int {
+    return id
+  }
+
+  static func == (left: Car, right: Car) {
+    return left.id == right.id
+      && left.name == right.name
+      && left.travelledMiles = right.travelledMiles
+  }
+}
